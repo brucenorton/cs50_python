41e6d173d22c1cdadc16e2561b65bbc704238253
Bruce Norton <bruce.norton@gmail.com>
2023-07-01 13:21:17 -0400

    small change

--- tunes.py
+++ tunes.py
@@ -7,8 +7,8 @@ if len(sys.argv) != 2:
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=9&term=" + sys.argv[1])
 ## print(json.dumps(response.json(),indent=2))
-o = response.json()
-for result in o["results"]:
+results = response.json()
+for result in results["results"]:
     print(result["artistName"] + " - " + result["trackName"] + " - " + result["collectionName"])
     # print(json.dumps(result,indent=
 #change
