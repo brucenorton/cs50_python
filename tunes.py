9ab10a434097558b7b962ebf61258b9e0a6f370d
Bruce Norton <bruce.norton@gmail.com>
2023-07-01 13:04:43 -0400

    added an ampersand

--- tunes.py
+++ tunes.py
@@ -4,6 +4,6 @@ import sys
 if len(sys.argv) != 2:
     sys.exit(1)
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
 print(response.json())
 #change
